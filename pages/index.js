43afa1ba12b42289dd0b56a697161b877f446a4f
Gulam Mohammad Mujtaba <89277268+gulam-mujtaba@users.noreply.github.com>
2023-04-16 23:47:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,246 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <!DOCTYPE html>
+<html>
+<head>
+	<title>Gulam Mohammad Mujtaba - Resume</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Gulam Mohammad Mujtaba</h1>
+		<p>Tel: +919700414783 | Email: gulammujtaba25@gmail.com</p>
+		<p>20-7-240/4, Qazipura St, Shah Ali Banda road, Hyderabad - 500065</p>
+		<a href="https://tap.bio/@gulam.mujtaba/">Website</a>
+	</header>
+	<main>
+		<section>
+			<h2>Objective</h2>
+			<p>Generalist with loads of experience in marketing, content creation,
+			design and nocode. A highly motivated individual honing variety of
+			powerful skills. Adept in various social media platforms, office
+			technology programs, automation tools and nocode builders.</p>
+		</section>
+		<section>
+			<h2>Work Experience</h2>
+			<table>
+				<tr>
+					<th>05/2022 - Present</th>
+					<td>Digiryte</td>
+					<td>Social Media Manager</td>
+					<td>
+						<ul>
+							<li>Scheduling daily posts on 3 major social media platforms - Facebook,
+							LinkedIn and Twitter.</li>
+							<li>Repurpose content on social media from inhouse published articles,
+							case studies and client stories.</li>
+							<li>Engage with audience on regular basis.</li>
+						</ul>
+					</td>
+				</tr>
+				<tr>
+					<th>05/2022 - 08/2022</th>
+					<td>Muslim Ameer</td>
+					<td>Social Media Executive</td>
+					<td>
+						<ul>
+							<li>Planning Social Media Weekly calender for Muslim Ameer social media
+							handles.</li>
+							<li>Content Creation and Social Media Management for company pages
+							on LinkedIn and Instagram.</li>
+							<li>Video editing and graphic design for planned social media content.</li>
+						</ul>
+					</td>
+				</tr>
+				<tr>
+					<th>04/2022 - 05/2022</th>
+					<td>HBE Capital</td>
+					<td>Marketing and Analytics Intern</td>
+					<td>
+						<ul>
+							<li>Planning, Designing content, social media campaigns for launch of HBE
+							Capital Smallcase products.</li>
+							<li>Creating HBE Capital social media brand presence across all Social
+							media pages.</li>
+							<li>Worked on analytics and data presentation for HBE Capital using Excel
+							sheet &amp; Datawrapper.</li>
+						</ul>
+					</td>
+				</tr>
+				<tr>
+					<th>11/2021 - 01/2022</th>
+					<td>The Logical Investor - Youtube Channel</td>
+					<td>YouTube Video Editor</td>
+					<td>
+						<ul>
+							<li>Editing Youtube Videos on weekly basis.</li>
+							<li>Making Videos more interactive with custom infographics and
+							overlays.</li>
+							<li>Designing engaging thumbnails.</li>
+						</ul>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+</div>
+			</div>
+			<div class="resume-item">
+				<h3 class="resume-section">Education</h3>
+				<div class="resume-item-content">
+					<h4>Bachelor of Engineering in Electrical and Electronics</h4>
+					<p>Deccan College of Engineering and Technology - Affiliated to Osmania University</p>
+					<ul>
+						<li>Completed in June 2022</li>
+						<li>GPA: 3.5/4.0</li>
+						<li>Relevant coursework: Computer Networks, Digital Signal Processing, Control Systems</li>
+					</ul>
+					<h4>No-Code Developer Bootcamp</h4>
+					<p>Airdev.Co</p>
+					<ul>
+						<li>Completed in April 2022</li>
+						<li>Learned how to develop web applications without code using Bubble.io and Zapier</li>
+					</ul>
+					<h4>High School</h4>
+					<p>Gowtham Model School</p>
+					<ul>
+						<li>Completed in May 2016</li>
+						<li>CGPA: 9.0/10.0</li>
+					</ul>
+				</div>
+			</div>
+			<div class="resume-item">
+				<h3 class="resume-section">Skills</h3>
+				<div class="resume-item-content">
+					<ul>
+						<li>Social Media Design</li>
+						<li>Social Media Management</li>
+						<li>Email Campaign Management</li>
+						<li>Facebook Ads</li>
+						<li>No-Code development</li>
+						<li>Video Editing</li>
+						<li>Microsoft/Google Work Tools</li>
+						<li>CRM Software</li>
+					</ul>
+				</div>
+			</div>
+			<div class="resume-item">
+				<h3 class="resume-section">Interests</h3>
+				<div class="resume-item-content">
+					<ul>
+						<li>Nocode/lowcode development</li>
+						<li>Content creation</li>
+						<li>Web3</li>
+						<li>Technology</li>
+						<li>Startups</li>
+						<li>Personal Branding</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
 
-      <Footer />
-    </div>
+body {
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+  color: #333;
+  margin: 0;
+  padding: 0;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: "Arial Black", Arial, sans-serif;
+  font-weight: 700;
+  color: #333;
+  margin: 0 0 10px;
+  padding: 0;
+}
+
+h1 {
+  font-size: 32px;
+}
+
+h2 {
+  font-size: 24px;
+}
+
+h3 {
+  font-size: 20px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 10px;
+}
+
+table {
+  border-collapse: collapse;
+  margin-bottom: 20px;
+}
+
+th, td {
+  padding: 8px;
+  border: 1px solid #ddd;
+}
+
+th {
+  background-color: #f2f2f2;
+  font-weight: bold;
+  text-align: left;
+}
+
+.skills {
+  margin-bottom: 20px;
+}
+
+.skills h3 {
+  margin-bottom: 10px;
+}
+
+.skills ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.skills li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  padding: 5px 10px;
+  background-color: #f2f2f2;
+  border-radius: 5px;
+}
+
+.activities {
+  margin-bottom: 20px;
+}
+
+.activities h3 {
+  margin-bottom: 10px;
+}
+
+.activities ul {
+  margin-left: 20px;
+}
+
+.activities li {
+  margin-bottom: 5px;
+}
+
+
+
+
+
+
+
   )
 }
